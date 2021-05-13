21223136f73fe37e7aee6b7b3a8258415b8102a6
Allan Lu <allanluser@gmail.com>
2021-05-12 20:03:42 -0400

    add express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const server = express()
+
+const { PORT = 3000 } = process.env
+
+server.use(express.static(__dirname))
+
+server.listen(PORT, () => {
+  console.log("I am listening...")
+})
