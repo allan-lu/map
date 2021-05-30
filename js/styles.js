a53c40f5acc670e0eb4cd58cba5b0c543a13d05e
Allan Lu <allanluser@gmail.com>
2021-05-30 01:05:53 -0400

    add favicon

--- js/styles.js
+++ js/styles.js
@@ -22,8 +22,8 @@ const myStyle = {
   },
   selectAndHighlight: {
     weight: 2,
-    color: "#c482b9",
-    fillColor: "#e872e8"
+    color: "#8b4596",
+    fillColor: "#d234eb"
   },
   csaChoro(feature) {
     return {
